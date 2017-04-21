8c62137698bb5337c1b520ede55e706e4f6a7384
Biokinetix <biokinetix@hotmail.com>
2017-04-21 16:22:48 -0500

    Update bash-init.sh

--- bash-init.sh
+++ bash-init.sh
@@ -1,2 +1,2 @@
-ulimit -c unlimited; java -Xmx2G -Xms256M -XX:+AggressiveOpts -Xmn128M -jar spigot.jar -o false
+java -Xmx2G -Xms256M -XX:+AggressiveOpts -Xmn128M -jar spigot.jar -o false
 echo "Curl from https://github.com/SergioCasares/init-pot"
